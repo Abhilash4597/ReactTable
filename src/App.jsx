26c71cf504193e032697d88703c66a2148d74b59
Abhilash4597 <abhilash4597@gmail.com>
2023-04-04 19:14:35 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,63 @@
+import React, { useState } from 'react';
+import { data } from './Components/Data';
+// import './App.css';
+
+function App() {
+  // const [contacts, setContacts] = useState(data);
+  const [search, setSearch] = useState('');
+
+  // const sortName = () => {
+  //   setContacts(
+  //     data.sort((a, b) => {
+  //       return a.first_name.toLowerCase() < a.first_name.toLowerCase()
+  //         ? -1
+  //         : a.first_name.toLowerCase() > a.first_name.toLowerCase()
+  //         ? 1
+  //         : 0;
+  //     })
+  //   );
+  // };
+
+  return (
+    <div>
+      <h1 className="text-center mt-4">Personal Information Table</h1>
+      <form>
+        {/* onChange for search */}
+
+        <input
+          type="text"
+          onChange={e => setSearch(e.target.value)}
+          placeholder="Search contacts"
+        />
+      </form>
+      <table>
+        <thead>
+          <tr>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Email</th>
+            <th>Phone</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data
+            .filter(item => {
+              return search.toLowerCase() === ''
+                ? item
+                : item.first_name.toLowerCase().includes(search);
+            })
+            .map((item, index) => (
+              <tr key={index}>
+                <td>{item.first_name}</td>
+                <td>{item.last_name}</td>
+                <td>{item.email}</td>
+                <td>{item.phone}</td>
+              </tr>
+            ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default App;
